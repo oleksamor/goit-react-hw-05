9e976c55fea5feafe9665aa50e1045788f25de06
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-08-23 14:18:52 +0300

    add Router and create routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import Header from "./components/Header/Header";
 // import { Routes, Route, NavLink } from "react-router-dom";
 import clsx from "clsx";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { Route, Routes } from "react-router-dom";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div>
       <Header />
-      {/* <MovieList /> */}
-      {/* <NotFoundPage /> */}
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/MovieList" element={<h2>Movies Page</h2>} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import s from "./Header.module.css";
 import clsx from "clsx";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <nav>
-        <ul className={s.nav}>
-          <li>Home</li>
-          <li>Movies</li>
+      <nav className={s.nav}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/MovieList">Movies</Link>
+          </li>
         </ul>
       </nav>
     </header>
