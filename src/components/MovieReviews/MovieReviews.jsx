37d6063cb1c1e6bcbf6d80368d593968d5b849c9
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-08-24 17:49:54 +0300

    add route, styles in MovieReviews

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import s from "./MovieReviews.module.css";
+import { fetchReviewsById } from "../../services/api";
 
 const MovieReviews = () => {
-  return <div>MovieReviews</div>;
+  const [review, setReview] = useState([]);
+  const params = useParams();
+
+  useEffect(() => {
+    fetchReviewsById(params.movieId).then((data) => setReview(data));
+  }, [params.movieId]);
+  return (
+    <div>
+      <ul className={s.list}>
+        {review.map((item) => (
+          <li key={item.id} className={s.listItem}>
+            <p className={s.author}>Author: {item.author}</p>
+            <p>{item.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieReviews;
